cb79fc69bc6ff801f62f6844349af52f264d1763
Autumn Hazel <autumn.hazel91@gmail.com>
2025-03-31 07:58:24 -0400

    Filling out graphics with mesh and shader functions

--- gfx.c
+++ gfx.c
@@ -9,10 +9,8 @@ GLFWwindow* ACTIVE_WINDOW = NULL;
 
 GLFWwindow* gfxQuickWindowCreate(int width, int height, const char* title) {
 	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
-	if (!window) {
+	if (!window)
 		printf("[E]\t In window_create(): Failed to open window :("); // REPLACE ALL OF THESE WITH LOG EVENTUALLY
-	}
-
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, &_DEFAULT_FRAMEBUFFER_CALLBACK);
 	glfwSetWindowCloseCallback(window, &_DEFAULT_WINDOW_CLOSE_CALLBACK);
@@ -24,6 +22,7 @@ GLFWwindow* gfxQuickWindowCreate(int width, int height, const char* title) {
 		printf("Failed to initialize glad :(");
 	}
 
+	// REMOVE DEBUG
 	fprintf(stderr, "RESULT OF FUNCTION: %x\n",stbi_is_16_bit("assets/sprites/baphomet.png"));
 
 	ACTIVE_WINDOW = window;
@@ -34,10 +33,69 @@ GLFWwindow* gfxGetActiveWindow() {
 	return ACTIVE_WINDOW;
 }
 
+GFXmesh gfxGenerateRect(float w, float h) {
+	GFXmesh mesh = gfxMeshStart(VERTEX_FORMAT_UV);
+	float vert[4*5] = {
+		-w/2,	 h/2, 0.0f, 0.0f, 1.0f,
+		 w/2,	 h/2, 0.0f, 1.0f, 1.0f,
+		 w/2,	-h/2, 0.0f, 1.0f, 0.0f,
+		-w/2,	-h/2, 0.0f, 0.0f, 0.0f};
+	int indcs[6] = {3,0,1,1,2,3};
+	gfxMeshAddVertices(mesh, GL_STATIC_DRAW, vert, 4*5, indcs, 6);
+	gfxMeshFinish(mesh);
+	return mesh;
+}
+
+GFXmesh gfxMeshStart(int format) {
+	GFXmesh mesh = {0, 0, format};
+	glGenVertexArrays(1, &mesh.vao);
+	glBindVertexArray(mesh.vao);
+	return mesh;
+}
+
+void gfxMeshAddVertices(GFXmesh mesh, GLenum usage_mode, float* vertices, size_t nv, int* indices, size_t ni) {
+	unsigned int vbo, ebo, nelem;
+	nelem = 3 + (mesh.format&VERTEX_FORMAT_UV)*2 + (mesh.format&VERTEX_FORMAT_RBG)*3 + (mesh.format&VERTEX_FORMAT_ALPHA); // Fight me cowards
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * nv * mesh.format, vertices, usage_mode);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * ni, indices, usage_mode);
+	mesh.elements = ni;
+}
+
+void gfxMeshFinish(GFXmesh mesh) {
+	unsigned int stride = 3 + (mesh.format&VERTEX_FORMAT_UV)*2 + (mesh.format&VERTEX_FORMAT_RBG)*3 + (mesh.format&VERTEX_FORMAT_ALPHA);
+	int index;
+	glVertexAttribPointer(index, 3, GL_FLOAT, GL_FALSE, stride*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(index);
+	if (mesh.format&VERTEX_FORMAT_UV)
+		glVertexAttribPointer(index++, 2, GL_FLOAT, GL_FALSE, stride*sizeof(float), (void*)(3*sizeof(float)));
+		glEnableVertexAttribArray(index);
+	if (mesh.format&VERTEX_FORMAT_RBG)
+		glVertexAttribPointer(index++, 3+(mesh.format&VERTEX_FORMAT_ALPHA), GL_FLOAT, GL_FALSE, stride*sizeof(float), (void*)((3+(mesh.format&VERTEX_FORMAT_UV)*2)*sizeof(float)));
+		glEnableVertexAttribArray(index);
+}
+
+GFXshader gfxCreateShader(GFXsfrag* fragments) {
+
+}
+
+GFXsfrag gfxBuildShaderFragment(char* filepath, int type) {
+	GLchar* shader_source;
+	char info_log[512];
+	int success;
+
+	unsigned int shader = glCreateShader(type);
+	glShaderSource(shader, 1, (const char**)&SHADERSOURCE, NULL);
+	glCompileShader(shader);
+}
+
 void _DEFAULT_WINDOW_CLOSE_CALLBACK(GLFWwindow* window) {
-	//glfwSetWindowShouldClose(window, 1);
+	glfwSetWindowShouldClose(window, 1);
 }
 
 void _DEFAULT_FRAMEBUFFER_CALLBACK(GLFWwindow* window, int width, int height) {
-	//glViewport(0, 0, width, height); // Directly scales window when resized :3
+	glViewport(0, 0, width, height); // Directly scales window when resized :3
 }
--- gfx.h
+++ gfx.h
@@ -25,6 +25,12 @@
 
 /* macros */
 
+/* vertex formats*/
+#define VERTEX_FORMAT_UV	0x1 << 0
+#define VERTEX_FORMAT_RBG	0x1 << 1
+#define VERTEX_FORMAT_ALPHA	0x1 << 2
+
+
 /* types */
 typedef struct {
 	unsigned int texture;
@@ -32,9 +38,17 @@ typedef struct {
 } GFXtexture;
 
 typedef struct {
+	unsigned int vao;
+	unsigned int elements;
+	int 		 format;
+} GFXmesh;
+
+typedef struct {
 	unsigned int* 	shader;
 	unsigned int	program;
 } GFXshader;
+
+typedef unsigned int GFXsfrag; 
 
 /* window */
 GLFWwindow* gfxQuickWindowCreate(int width, int height, const char* title); // Implement monitor stuff later maybe? Open in a certain monitor by default? Idk??
@@ -50,6 +64,16 @@ void gfxSetShader();
 void gfxGetShader();
 void gfxSetBlendmode();
 
+/* mesh primitives */
+GFXmesh gfxMeshStart(int format);
+void gfxMeshAddVertices(GFXmesh mesh, GLenum usage_mode, float* vertices, size_t nv, int* indices, size_t ni);
+void gfxMeshFinish(GFXmesh mesh);
+GFXmesh gfxGenerateRect(float w, float h);
+GFXmesh gfxGenerateCirc(float r, float subdiv);
+
+/* shaders */
+GFXshader gfxCreateShader(GFXsfrag* fragments); 
+GFXsfrag gfxBuildShaderFragment(char* filepath, int type);
 
 /* default callbacks */
 void _DEFAULT_WINDOW_CLOSE_CALLBACK(GLFWwindow* window);
